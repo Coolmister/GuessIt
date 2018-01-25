2e20da4b33518b93e7f7ee442d01d35143b98244
DAMIAN HERNANDEZ <damherna@uat.edu>
2018-01-25 13:04:25 -0700

    First check in?

--- GuessIt/GuessIt.cpp
+++ GuessIt/GuessIt.cpp
@@ -18,11 +18,14 @@ int main()
 
 	cout << "\tWelcome to Guess It\n\n";
 
-	do {
+	do
+	{
+		//asks for a user integer input
 		cout << "Enter a Number: ";
 		cin >> guess;
 		++tries;
 
+		//if conditions
 		if (guess > SecretNumber)
 		{
 			cout << "Too High!!\n\n";
@@ -33,9 +36,9 @@ int main()
 		}
 		else
 		{
-			cout << "\nThat's the number! Good job you got it in " << tries << " tries!";
+			cout << "\nThat's the number! Good job you got it in " << tries << " tries!\n";
 		}
-	}
+	} while (guess != SecretNumber);
 
     return 0;
 }
